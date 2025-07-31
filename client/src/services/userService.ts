5a9bd189f8d8b626123b7faee7e09ac512f8d2f5
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2025-07-31 12:52:35 +0530

    Integrate GET & PATCH API's

---
+++ client/src/services/userService.ts
@@ -0,0 +1,31 @@
+import { EditUserProfileRequest } from "@/types/userProfile";
+
+interface EditUserProfileParams {
+  token: string;
+  requestBody: EditUserProfileRequest;
+}
+
+export const editUserProfile = async (
+  token: string,
+  requestBody: EditUserProfileRequest
+): Promise<void> => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/users/user-profile`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(requestBody),
+    }
+  );
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Failed to update profile");
+  }
+
+  console.log("✅ Profile updated:", data.profile);
+};
---
+++ client/src/types/userProfile.ts
@@ -0,0 +1,30 @@
+export type PersonalDetails = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  dob?: string;
+};
+
+export type ContactInformation = {
+  email: string;
+  phone: string;
+  city?: string;
+  country?: string;
+};
+
+export type ProfessionalDetails = {
+  occupation?: string;
+  website?: string;
+};
+
+export type AdditionalInformation = {
+  statusMessage?: string;
+  bio?: string;
+};
+
+export interface EditUserProfileRequest {
+  personal_details: PersonalDetails;
+  contact_information: ContactInformation;
+  professional_details: ProfessionalDetails;
+  additional_information: AdditionalInformation;
+}
